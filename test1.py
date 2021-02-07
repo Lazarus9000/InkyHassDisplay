3844440fe15a254d1b945e4a75b1c8e32063f235
Lars Knudsen <jegharen@gmail.com>
2021-02-07 22:19:32 +0100

    Update test1.py
    order of defs test

--- test1.py
+++ test1.py
@@ -8,6 +8,24 @@ import os
 #Get current path
 PATH = os.path.dirname(__file__)
 
+def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
+    """Create a transparency mask.
+    Takes a paletized source image and converts it into a mask
+    permitting all the colours supported by Inky pHAT (0, 1, 2)
+    or an optional list of allowed colours.
+    :param mask: Optional list of Inky pHAT colours to allow.
+    """
+    mask_image = Image.new("1", source.size)
+    w, h = source.size
+    for x in range(w):
+        for y in range(h):
+            p = source.getpixel((x, y))
+            if p in mask:
+                mask_image.putpixel((x, y), 255)
+
+    return mask_image
+
+
 #Read token
 f = open("token.txt", "r")
 token = f.read().strip()
@@ -57,19 +75,3 @@ img.paste(inky_display.WHITE, (10, 10), iconmask)
 #Success!
 
 
-def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
-    """Create a transparency mask.
-    Takes a paletized source image and converts it into a mask
-    permitting all the colours supported by Inky pHAT (0, 1, 2)
-    or an optional list of allowed colours.
-    :param mask: Optional list of Inky pHAT colours to allow.
-    """
-    mask_image = Image.new("1", source.size)
-    w, h = source.size
-    for x in range(w):
-        for y in range(h):
-            p = source.getpixel((x, y))
-            if p in mask:
-                mask_image.putpixel((x, y), 255)
-
-    return mask_image
